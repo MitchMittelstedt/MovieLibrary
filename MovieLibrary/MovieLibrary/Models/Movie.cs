e6057f3f3d89b6f1eb84e6205455e11fb3bc5df2
cascando <mmittelstedt@icloud.com>
2019-06-11 13:29:04 -0500

    added Movies database and seeded data

--- MovieLibrary/MovieLibrary/Models/Movie.cs
+++ MovieLibrary/MovieLibrary/Models/Movie.cs
@@ -1,14 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace MovieLibrary.Models
 {
+
     public class Movie
     {
-        public string title;
-        public string genre;
-        public string director;
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+        [Display(Name = "Genre")]
+        public string Genre { get; set; }
+        [Display(Name = "Director Name")]
+        public string DirectorName { get; set; }
+        [Display(Name = "Image")]
+        public string Image { get; set; }
     }
 }
