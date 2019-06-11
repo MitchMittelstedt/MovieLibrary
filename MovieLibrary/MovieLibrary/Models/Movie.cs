4540ed53116dab227251007d02425faac765ab4e
cascando <mmittelstedt@icloud.com>
2019-06-11 11:52:35 -0500

    added model class with three properties

---
+++ MovieLibrary/MovieLibrary/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieLibrary.Models
+{
+    public class Movie
+    {
+        public string title;
+        public string genre;
+        public string director;
+    }
+}
